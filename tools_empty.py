a6100583fdb72f0ea276afceb8978fd644794bc3
Peter Lebiedzinski <63629309+plebbyd@users.noreply.github.com>
2020-10-27 21:50:54 -0500

    Add files via upload

---
+++ tools_empty.py
@@ -0,0 +1,49 @@
+
+class DataCropper():
+
+    def __init__(self):
+
+
+    def load(self):
+
+        return True
+
+
+    def max(self):
+        return True
+
+    def run(self):
+
+        return True
+
+
+    def get_maxmin(self):
+
+        return True
+
+    def maxmin(self):
+        return True
+
+
+    def get_boxes(self):
+
+        return boxes
+
+
+    def visualize_group(self):
+
+        return True
+
+    def find_local(self):
+
+
+        return results
+
+    def sort_boxes(self):
+
+    return True
+
+    def fit_boxes(self):
+
+
+        return True
